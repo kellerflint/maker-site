e285a6f04595ad0c68842f6020320784af1e8f14
kellerflint <kflint0068@gmail.com>
2019-01-28 07:56:20 -0800

    added default rank to new user

--- instructor/create.php
+++ instructor/create.php
@@ -14,7 +14,22 @@ $insert = "insert into User values (default, '" . $user_name . "', '" . $first_n
 $result = mysqli_query($db, $insert);
 
 if ($result) {
+    $get_new_user = 'select user_id from User where user_name = "' . $user_name . '";';
+    $user_id_set = mysqli_query($db, $get_new_user);
+    $user_id_item = mysqli_fetch_assoc($user_id_set);
+    $user_id = $user_id_item['user_id'];
+
+    $get_unranked = 'select rank_id from Rank where rank_title = "Unranked";';
+    $unranked_set = mysqli_query($db, $get_unranked);
+    $unranked_item = mysqli_fetch_assoc($unranked_set);
+    $unranked_id = $unranked_item['rank_id'];
+    
+    $default_rank = 'insert into User_Rank values (' . $user_id . ', ' . $unranked_id . ', now());';
+
+    $rank_result = mysqli_query($db, $default_rank);
+
     header('Location: ' . WWW_ROOT . '/student/leaderboard.php'); 
+
 } else {
     // Insert failed
     echo mysqli_error($db);
